0d77e8164a2c3a73617f790a13f8eb528d747029
Sakkar Chowdhury <Sakkhor.Chowdhury@gmail.com>
2024-03-25 15:15:39 +0600

    Latest News are added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
+import LatestNews from "@/components/ui/LatestNews/LatestNews";
+import { Grid } from "@mui/material";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to New HomePage</h1>
-      
-    </div>
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <LatestNews/>
+        </Grid>
+        <Grid item xs={4}>
+          <h1>SideBar</h1>
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
